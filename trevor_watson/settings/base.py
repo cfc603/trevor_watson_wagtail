e5fa3acdecdef467ed565e132c699d79535c7c15
cfc603 <wtrevor162@gmail.com>
2017-09-18 08:36:36 -0400

    set database to MySQL

--- trevor_watson/settings/base.py
+++ trevor_watson/settings/base.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'trevor_watson.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trevor_watson_wagtail',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
