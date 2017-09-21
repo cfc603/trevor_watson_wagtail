96fce8b8d62190a9e6331063a33eb6daaf6c71fc
cfc603 <wtrevor162@gmail.com>
2017-09-21 17:53:13 -0400

    buildout home.models.HomePage & migrations

--- home/models.py
+++ home/models.py
@@ -2,8 +2,13 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
-    pass
+    subtitle = models.CharField(max_length=255, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+    ]
