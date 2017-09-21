4a83b7091f2a32e1e3fa21957f4205d79ed55ec1
cfc603 <wtrevor162@gmail.com>
2017-09-21 18:28:39 -0400

    add home.models.AboutPage, migrations & template

--- home/models.py
+++ home/models.py
@@ -3,7 +3,24 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailsearch import index
+
+
+class AboutPage(Page):
+    subtitle = models.CharField(max_length=255, blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle", classname="full"),
+        FieldPanel("body", classname="full")
+    ]
+
+    search_fields = Page.search_fields + [
+        index.SearchField("subtitle"),
+        index.SearchField("body")
+    ]
 
 
 class HomePage(Page):
