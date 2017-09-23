738dcbcee20bf8dfdac1a2ede1b5a8e5bdbe1adc
cfc603 <wtrevor162@gmail.com>
2017-09-23 13:29:04 -0400

    remove home.model.AboutPage as well as associated migrations and template

--- home/models.py
+++ home/models.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
-from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsearch import index
 from wagtail.wagtailsnippets.models import register_snippet
@@ -12,21 +11,6 @@ from modelcluster.fields import ParentalKey
 
 
 # Pages
-class AboutPage(Page):
-    subtitle = models.CharField(max_length=255, blank=True)
-    body = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel("subtitle", classname="full"),
-        FieldPanel("body", classname="full")
-    ]
-
-    search_fields = Page.search_fields + [
-        index.SearchField("subtitle"),
-        index.SearchField("body")
-    ]
-
-
 class HomePage(Page):
     subtitle = models.CharField(max_length=255, blank=True)
 
