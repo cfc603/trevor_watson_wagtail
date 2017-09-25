d4ec21c0678d0450babb93b71e793c8b186fa156
cfc603 <wtrevor162@gmail.com>
2017-09-25 16:38:23 -0400

    add ProjectTag as well as needed migrations and templates

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,18 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 from wagtail.wagtailadmin.edit_handlers import (
-    FieldPanel, InlinePanel, StreamFieldPanel
+    FieldPanel, InlinePanel, MultiFieldPanel, StreamFieldPanel
 )
 from wagtail.wagtailcore.fields import StreamField
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 from .blocks import ContentStreamBlock
 
 
-# Pages
 class ProjectImageCarousel(Orderable):
     image = models.ForeignKey(
         'wagtailimages.Image',
@@ -50,11 +51,22 @@ class ProjectPage(Page):
     subtitle = models.CharField(max_length=255, blank=True)
     body = StreamField(ContentStreamBlock())
     date = models.DateField()
+    tags = ClusterTaggableManager(through="ProjectTag", blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
+        MultiFieldPanel([
+            FieldPanel("date"),
+            FieldPanel("tags"),
+        ], heading="Project Information"),
         StreamFieldPanel("body"),
-        FieldPanel("date"),
         InlinePanel("carousel_images"),
         InlinePanel("links"),
     ]
+
+    def get_main_image(self):
+        return self.carousel_images.first().image
+
+
+class ProjectTag(TaggedItemBase):
+    content_object = ParentalKey("ProjectPage", related_name="tagged_items")
