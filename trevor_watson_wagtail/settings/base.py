911579c1008daa41727cda22ec0be322b8c5d620
cfc603 <wtrevor162@gmail.com>
2017-10-14 14:26:34 -0400

    rename from trevor_watson to trevor_watson_wagtail

--- trevor_watson_wagtail/settings/base.py
+++ trevor_watson_wagtail/settings/base.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'wagtail.wagtailredirects.middleware.RedirectMiddleware',
 ]
 
-ROOT_URLCONF = 'trevor_watson.urls'
+ROOT_URLCONF = 'trevor_watson_wagtail.urls'
 
 SECRET_KEY = get_secrets('SECRET_KEY')
 
@@ -157,7 +157,7 @@ MEDIA_URL = '/media/'
 # Wagtail settings
 
 WAGTAILIMAGES_JPEG_QUALITY = 60
-WAGTAIL_SITE_NAME = "trevor_watson"
+WAGTAIL_SITE_NAME = "trevor_watson_wagtail"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
