8631db748e6f4ebb94f66b896633e13836ff3317
cfc603 <wtrevor162@gmail.com>
2017-10-18 21:29:52 -0400

    update tag_nav for cleaner syntax

--- portfolio/templatetags/portfolio_tags.py
+++ portfolio/templatetags/portfolio_tags.py
@@ -8,14 +8,13 @@ register = template.Library()
 
 @register.inclusion_tag("portfolio/tags/tag_nav.html", takes_context=True)
 def tag_nav(context, count=None):
+    tags = ProjectPage.tags.annotate(
+        project_count=Count("portfolio_projecttag_items__content_object")
+    )
+    tags = tags.order_by("-project_count", "name")
+
     if count:
-        tags = ProjectPage.tags.annotate(
-            project_count=Count("portfolio_projecttag_items")
-        ).order_by("-project_count", "name")[:count]
-    else:
-        tags = ProjectPage.tags.annotate(
-            project_count=Count("portfolio_projecttag_items")
-        ).order_by("-project_count", "name")
+        tags = tags[:count]
 
     return {
         "current_tag": context["request"].GET.get("tag"),
